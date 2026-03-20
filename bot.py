ffd7bf25123143605b90c66d98cfab864d49ad2e
Peter <berestnev.p@yandex.ru>
2026-03-20 19:16:06 +0400

    Зависимости

--- bot.py
+++ bot.py
@@ -93,13 +93,6 @@ def apply_proxy_from_env() -> None:
 
 apply_proxy_from_env()
 
-# Настройка логирования
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-
 # Инициализация базы данных
 db = Database()
 
